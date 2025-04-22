15461d4912134fc9946cf9f817da17f8bba7c3bd
AndrewO13 <146053768+AndrewO13@users.noreply.github.com>
2025-04-22 03:15:23 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div className="content-section">
       <div className="profile-section">
-        <img src={process.env.PUBLIC_URL + '/images/YoungM.E.jpg'} alt="Andrew" />
+        <img src={process.env.PUBLIC_URL + 'images/YoungM.E.jpg'} alt="Andrew" />
       </div>
     </div>
   );
