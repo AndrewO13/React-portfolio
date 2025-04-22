0d3f70b5156f0ffaa9157d2033bee66bc1b5d907
AndrewO13 <146053768+AndrewO13@users.noreply.github.com>
2025-04-22 03:10:59 +0300

    Update Hobby.jsx

--- src/components/Hobby/Hobby.jsx
+++ src/components/Hobby/Hobby.jsx
@@ -7,7 +7,7 @@ const Hobby = () => {
       <h2>Hobby Video</h2>
       <div className="video-container">
         <video controls>
-          <source src="Madhesi.mp4" type="video/mp4" />
+          <source src={process.env.PUBLIC_URL + '/videos/Madhesi.mp4'} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
